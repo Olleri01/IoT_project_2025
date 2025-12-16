fc63efc4219121ee96d68a02130004b86455c5c0
Ara <a.haulos@gmail.com>
2025-12-17 00:37:28 +0200

    pedestrian counter

---
+++ src/object_detection_test/object_detection.py
@@ -0,0 +1,128 @@
+import struct
+import time
+import json
+import aesio
+import os
+
+class object_detection_client:
+    def __init__(self, sock_pool, ssl_context, host_ip, host_port0, host_port1):
+        self.host_ip = host_ip
+        self.host_port0 = host_port0
+        self.host_port1 = host_port1
+        self.pool = sock_pool
+        self.ssl_context = ssl_context
+        self.sock = None
+        
+        
+    def send_data(self, sock, aes, data):
+        if (aes == None):
+            sock.sendall(data)
+        else:
+            buffer = bytearray(len(data))
+            aes.encrypt_into(data, buffer)
+            sock.sendall(buffer)
+        
+    def receive_data(self, sock, aes, num_of_bytes):
+        if (aes == None):
+            data = bytearray(num_of_bytes)
+            sock.recv_into(data, num_of_bytes)
+            return data
+        else:
+            buffer = bytearray(num_of_bytes)
+            data = bytearray(num_of_bytes)
+            
+            sock.recv_into(buffer, num_of_bytes)
+            aes.decrypt_into(buffer, data)
+            
+            return data
+        
+    def receive_string(self, sock, aes):
+        string_len, = struct.unpack("<I", self.receive_data(sock, aes, struct.calcsize("<I")))
+        return self.receive_data(sock, aes, string_len).decode("utf-8")
+    
+    def send_string(self, sock, aes, str):
+        self.send_data(sock, aes, struct.pack("<I", len(str)))
+        self.send_data(sock, aes, str.encode('utf-8'))
+        
+    
+    def receive_session_keys(self):
+        ssl_socket = self.ssl_context.wrap_socket(self.pool.socket(), server_hostname="coursework")
+        ssl_socket.connect((self.host_ip, self.host_port0))
+        
+        token = bytearray(16)
+        key = bytearray(16)
+        
+        ssl_socket.recv_into(token, 16)
+        ssl_socket.recv_into(key, 16)
+        ssl_socket.close()
+        
+        return (token, key)
+        
+        
+    def connect(self):
+        sock = None
+                
+        try:
+            if (self.ssl_context == None):
+                self.aes = None
+                
+                sock = self.pool.socket(self.pool.AF_INET, self.pool.SOCK_STREAM)
+                sock.connect((self.host_ip, self.host_port1))
+                sock.sendall(b'unsafeplaintext')
+            else:
+                (token, key) = self.receive_session_keys()
+
+                self.aes = aesio.AES(key, aesio.MODE_CTR)
+                
+                sock = self.pool.socket(self.pool.AF_INET, self.pool.SOCK_STREAM)
+                sock.connect((self.host_ip, self.host_port1))
+                sock.sendall(token)
+        except Exception as e:
+            sock = None
+            pass
+            
+        return sock 
+        
+    def send_image(self, framebuffer, colorspace, width, height, frame_number):
+        if (self.sock == None):
+            self.sock = self.connect()
+            if (self.sock == None):
+                return
+        
+        try:
+            self.send_string(self.sock, self.aes, "send_image")
+            self.send_data(self.sock, self.aes, struct.pack("<IIIII",
+                                                            frame_number,
+                                                            len(framebuffer),
+                                                            colorspace,
+                                                            width,
+                                                            height))
+            self.send_data(self.sock, self.aes, framebuffer)
+        except Exception as e:
+            self.sock.close()
+            self.sock = None
+        
+        
+    def get_objects(self):
+        if (self.sock == None):
+            self.sock = self.connect()
+            if (self.sock == None):
+                return []
+            
+        objects = []
+        try:
+            self.send_string(self.sock, self.aes, "get_objects")
+            
+            num_of_bboxes, = struct.unpack("<I", self.receive_data(self.sock, self.aes, 4))
+            for i in range(num_of_bboxes):
+                bbox_json = self.receive_string(self.sock, self.aes)
+                objects.append(json.loads(bbox_json))
+            
+            
+        except Exception as e:
+            print(e)
+            self.sock.close()
+            self.sock = None
+        
+        return objects
+        
